db0c9b4ca0c4dc643f737f1b9c64127de141beff
unknown <mirvatbarakat1@gmail.com>
2023-03-13 17:39:45 +0200

    Added connection.php file

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+$host = "localhost";
+$db_user = "root";
+$db_pass = null;
+$db_name = "news_db";
+
+$conn = new mysqli($host, $db_user, $db_pass, $db_name);
+?>
